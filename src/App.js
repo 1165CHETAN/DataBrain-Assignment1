06921532f52d2247a33c4d2952f42af349c0091f
Chetan Chavan <111604779+1165CHETAN@users.noreply.github.com>
2024-02-02 22:43:02 +0530

    my-first-comment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import MainLayout from './pages/MainLayout.jsx';
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import Posts from './pages/Posts.jsx';
 import Products from './pages/Products.jsx';
-import SingleProduct from './pages/SingleProduct.jsx';
-import Error from './pages/Error.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -16,13 +10,7 @@ function App() {
 		<div className='App'>
 			<BrowserRouter>
 				<Routes>
-					<Route path='/' element={<MainLayout />}>
-						<Route index element={<Home />} />
-						<Route path='about' element={<About />} />
-						<Route path='products' element={<Products />} />
-						<Route path='products/:productId' element={<SingleProduct />} />
-						<Route path='posts' element={<Posts />} />
-						<Route path='*' element={<Error />} />
+					<Route path='/' element={<Products />}>
 					</Route>
 				</Routes>
 			</BrowserRouter>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -9,16 +9,11 @@ const Products = () => {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-lg-7">
-              <h1>Products</h1>
+              <h3 className="text-primary">👋Welcome to DataBrain | Assignment-2</h3>
             </div>
             <div className="col-lg-5">
               <nav>
-                <ol className="breadcrumb justify-content-end">
-                  <li className="breadcrumb-item">
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li className="breadcrumb-item active">Products</li>
-                </ol>
+                
               </nav>
             </div>
           </div>
@@ -40,7 +35,7 @@ const Products = () => {
                     <p className="card-text">{product.details}</p>
                     <div className="d-flex justify-content-between align-items-center">
                       <span>Price: <strong className="price">{product.price}</strong></span>
-                      <Link to={`/products/${product.id}`} className="btn btn-primary btn-sm">DETAILS &#8594;</Link>
+                      <Link to={`#`} className="btn btn-primary btn-sm">DETAILS &#8594;</Link>
                     </div>
                   </div>
                 </div>
